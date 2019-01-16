dbcbc1653c773296cd40351379c3a3895de456ec
sg3957 <sg3957@nyu.edu>
2019-01-16 09:41:17 -0500

    login auth, redux, routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+
+  def profile
+      render json: { user: UserSerializer.new(current_user()) }, status: :accepted
+    end
+
+
   def index
     @users = User.all
     render json: @users
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
   end
 
   def show
@@ -11,8 +29,9 @@ class Api::V1::UsersController < ApplicationController
 
   private
 
+  private
   def user_params
-    params.permit(:name)
+    params.require(:user).permit(:username, :password, :name)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :rooms
   has_many :furnitures, through: :rooms
+
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
 end
